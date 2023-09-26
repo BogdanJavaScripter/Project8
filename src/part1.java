253f9db6f6726a0372c0af6a81bfc2db33b78902
BogdanJavaScripter <bogdan.koltok@auk.edu.ua>
2023-09-26 11:10:24 +0300

    qwe

--- src/part1.java
+++ src/part1.java
@@ -2,16 +2,16 @@ public class part1 {
 
     public static void main(String[] args) {
         int n = 9;
-        System.out.println("Printing numbers backward:");
-        printNumberBackward(n);
+        System.out.println("Printing numbers:");
+        printNumber(n);
     }
 
-    public static void printNumberBackward(int n) {
+    public static void printNumber(int n) {
         if (n <= 0) {
-            return; //
+            return;
         }
+        printNumber(n - 1);
         System.out.print(n + " ");
-        printNumberBackward(n - 1);
     }
 }
 
